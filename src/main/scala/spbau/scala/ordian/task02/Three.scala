4b066fa9d63afaf20c989d5c91953efe4d84a3ec
Andronik Ordian <andronik.ordian@gmail.com>
2014-10-07 19:50:03 +0400

    case class -> case object

--- src/main/scala/spbau/scala/ordian/task02/Three.scala
+++ src/main/scala/spbau/scala/ordian/task02/Three.scala
@@ -2,10 +2,10 @@ package spbau.scala.ordian.task02
 
 object Three extends App {
 
-  assert(Not(Not(X())) == X())
-  assert(Not(Not(Not(X()))) == Or(Not(X()), And(Not(X()), Not(X()))))
-  assert(Not(And(True(), Not(False()))) == False())
-  assert(False() == And(And(Not(Or(X(), X())), X()), True()))
+  assert(Not(Not(X)) == X)
+  assert(Not(Not(Not(X))) == Or(Not(X), And(Not(X), Not(X))))
+  assert(Not(And(True, Not(False))) == False)
+  assert(False == And(And(Not(Or(X, X)), X), True))
 
   abstract class Bool {
     def ==(that: Bool): Boolean = simplify(this) equals simplify(that)
@@ -14,52 +14,52 @@ object Three extends App {
   case class And(left: Bool, right: Bool) extends Bool
   case class Or(left: Bool, right: Bool) extends Bool
   case class Not(op: Bool) extends Bool
-  case class True() extends Bool
-  case class False() extends Bool
-  case class X() extends Bool
+  case object True extends Bool
+  case object False extends Bool
+  case object X extends Bool
 
 
   /* clumsy */
   def simplify(b: Bool): Bool = b match {
     case And(left, right) => simplify(left) match {
-      case False() => False()
-      case True()  => simplify(right)
-      case X()     => simplify(right) match {
-        case False()  => False()
-        case True()   => X()
-        case Not(X()) => False()
-        case X()      => X()
+      case False => False
+      case True  => simplify(right)
+      case X     => simplify(right) match {
+        case False  => False
+        case True   => X
+        case Not(X) => False
+        case X      => X
       }
-      case Not(X())   => simplify(right) match {
-        case False()  => False()
-        case True()   => Not(X())
-        case Not(X()) => Not(X())
-        case X()      => False()
+      case Not(X)   => simplify(right) match {
+        case False  => False
+        case True   => Not(X)
+        case Not(X) => Not(X)
+        case X      => False
       }
     }
     case Or(left, right) => simplify(left) match {
-      case True()  => True()
-      case False() => simplify(right)
-      case X()     => simplify(right) match {
-        case True()   => True()
-        case False()  => X()
-        case Not(X()) => True()
-        case X()      => X()
+      case True  => True
+      case False => simplify(right)
+      case X     => simplify(right) match {
+        case True   => True
+        case False  => X
+        case Not(X) => True
+        case X      => X
       }
-      case Not(X())   => simplify(right) match {
-        case True()   => True()
-        case False()  => Not(X())
-        case Not(X()) => Not(X())
-        case X()      => True()
+      case Not(X)   => simplify(right) match {
+        case True   => True
+        case False  => Not(X)
+        case Not(X) => Not(X)
+        case X      => True
       }
     }
     case Not(op) => simplify(op) match {
-      case True()           => False()
-      case False()          => True()
+      case True           => False
+      case False          => True
       case Not(x)           => x
       case And(left, right) => simplify(Or(Not(left), Not(right)))
       case Or(left, right)  => simplify(And(Not(left), Not(right)))
-      case X()              => Not(X())
+      case X              => Not(X)
     }
     case x@_ => x
   }
